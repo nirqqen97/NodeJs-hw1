f636c70a54c81dbfb4b9b8d03f48b1c1b8d124c6
nirqqen97 <93587073+nirqqen97@users.noreply.github.com>
2023-03-07 16:00:08 +0200

    done

---
+++ contacts.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const fs = require('fs').promises;
+const shortid = require('shortid');
+
+ 
+ const contactsPath = path.join('db', 'contacts.json');
+  /**
+   *  func to get contacts
+   *  @param {Promise<JSON>}
+   */
+ const  listContacts = async () => {
+    const readRes = await fs.readFile(contactsPath);
+    const infoJs = JSON.parse(readRes)
+    console.table(infoJs)
+    return infoJs
+  }
+  /** get contacts by ID
+   * @param {Number} contactId 
+   */
+  
+  const getContactById = async (contactId) =>  {
+    const list = await Contacts()
+    
+    const contact = list.filter(item => item.id === `${contactId}`)
+    console.log('Contact: ', contact);
+  }
+  /** func to delete contacts by id 
+   * 
+   * @param {Number} contactId 
+   * @returns {Promise<Object>}
+   */
+  
+  const removeContact = async(contactId)  =>{
+    const list = await Contacts()
+    const index = list.findIndex((item) => item.id === contactId)
+    const removed = list.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(list, null, 2));
+    return removed[0];
+  }
+
+  /** func to add contact 
+   * 
+   * @param {string} name 
+   * @param {string} email 
+   * @param {string} phone 
+   * @returns {Promise<Object>}
+   */
+  
+  const addContact = async (name, email, phone) => {
+    const list = await Contacts();
+    const newContact = {
+      id: shortid.generate(),
+      name,
+      email,
+      phone
+    };
+    list.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(list, null, 2));
+    console.log('newContact: ', newContact);
+    return newContact;
+    
+  };
+
+  const Contacts = async() =>{
+    const readRes = await fs.readFile(contactsPath);
+    const infoJs = JSON.parse(readRes)
+    return infoJs
+  }
+
+  
+
+  
+module.exports = {  
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact
+
+}
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+https://ibb.co/ZYpd2JC
+https://ibb.co/MDvt3D2
+https://ibb.co/mBb8rww
+https://ibb.co/2WMgcYK
+https://ibb.co/LkzgwMC
